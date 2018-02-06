4ab333d856f3867f3586c66e435c1f1fbb76f34c
Osamah <oshareef84@gmail.com>
2018-02-05 20:16:39 -0600

    JUnit failure

--- src/main/java/persistence/UserDao.java
+++ src/main/java/persistence/UserDao.java
@@ -1,16 +1,10 @@
 package persistence;
 
 import entity.User;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.apache.logging.log4j.*;
+import org.hibernate.*;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Expression;
-import javax.persistence.criteria.Root;
+import javax.persistence.criteria.*;
 import java.util.List;
 
 public class UserDao {
--- src/test/java/test/util/Database.java
+++ src/test/java/test/util/Database.java
@@ -1,17 +1,10 @@
 package test.util;
 
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import org.apache.logging.log4j.*;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.*;
+import java.sql.*;
 import java.util.Properties;
 
 /**
