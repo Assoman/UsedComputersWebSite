ee6c8aab56dc8877d0065be729e873cc068e78d0
Osamah <oshareef84@gmail.com>
2018-02-05 19:19:30 -0600

    User hibernat

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,20 +1,49 @@
 package entity;
 
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * The type User.
+ * @author Osamah Shareef
+ * @since 2/5/2018
  */
+
+@Entity(name = "User")
+@Table(name = "users")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
     private int id;
+
+    @Column(name = "user_name")
     private String user_name;
+
+    @Column(name = "password")
     private String password;
+
+    @Column(name = "first_name")
     private String first_name;
+
+    @Column(name = "last_name")
     private String last_name;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "state")
     private String state;
+
+    @Column(name = "zipcode")
     private int zipcode;
+
+    @Column(name = "join_date")
     private LocalDate join_date;
+
+    @Column(name = "approved")
     private int approved;
 
     /**
