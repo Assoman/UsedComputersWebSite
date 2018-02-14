39d53cfe37d50e2a0edd020390c62049b724d227
assoman <oshareef84@gmail.com>
2018-02-13 18:34:08 -0600

    SQL updated

--- src/test/resources/cleandb.sql
+++ src/test/resources/cleandb.sql
@@ -1,2 +1,7 @@
 delete from users;
-INSERT INTO users VALUES (1,'jcoyne','supersecret1','Joe','Coyne','j@madison.com','Madison','Wisconsin','53704','2018-02-01','0'),(2,'fhensen','supersecret2','Fred','Hensen','f@madison.com','Madison','Wisconsin','53704','1988-05-08','0'),(3,'bcurry','supersecret3','Barney','Curry','b@madison.com','Madison','Wisconsin','53704','1947-11-11','0'),(4,'kmack','supersecret4','Karen','Mack','k@madison.com','Madison','Wisconsin','53704','1986-07-08','0'),(5,'dklein','supersecret5','Dianne','Klein','d@madison.com','Madison','Wisconsin','53704','1991-09-22','0'),(6,'dtillman','supersecret6','Dawn','Tillman','da@madison.com','Madison','Wisconsin','53704','1979-08-30','0');
+INSERT INTO users VALUES (1,'jcoyne','supersecret1','Joe','Coyne','j@madison.com','Madison','Wisconsin','53704','2018-02-01','0');
+INSERT INTO users VALUES (2,'fhensen','supersecret2','Fred','Hensen','f@madison.com','Madison','Wisconsin','53704','1988-05-08','0');
+INSERT INTO users VALUES (3,'bcurry','supersecret3','Barney','Curry','b@madison.com','Madison','Wisconsin','53704','1947-11-11','0');
+INSERT INTO users VALUES (4,'kmack','supersecret4','Karen','Mack','k@madison.com','Madison','Wisconsin','53704','1986-07-08','0');
+INSERT INTO users VALUES (5,'dklein','supersecret5','Dianne','Klein','d@madison.com','Madison','Wisconsin','53704','1991-09-22','0');
+INSERT INTO users VALUES (6,'dtillman','supersecret6','Dawn','Tillman','da@madison.com','Madison','Wisconsin','53704','1979-08-30','0');
