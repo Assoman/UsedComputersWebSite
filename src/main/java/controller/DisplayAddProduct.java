878236f1bc210edc867624a7bec0ad6bbd407acb
Osamah <oshareef84@gmail.com>
2018-04-14 16:50:26 -0500

    Protect add product from unapproved sellers

--- src/main/java/controller/DisplayAddProduct.java
+++ src/main/java/controller/DisplayAddProduct.java
@@ -1,5 +1,8 @@
 package controller;
 
+import entity.User;
+import persistence.GenericDao;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(
         name = "displayAddProduct",
@@ -17,9 +21,18 @@ public class DisplayAddProduct extends HttpServlet {
     public void doGet (HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
         HttpSession session = request.getSession();
+        GenericDao user = new GenericDao(User.class);
+        List<User> userIDList = user.getUserID(request.getRemoteUser());
         String url = "/addProduct.jsp";
+        String errorUrl = "/error.jsp";
         session.removeAttribute("AddedMessage");
-        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
-        dispatcher.forward(request, response);
+        if (userIDList.get(0).getApproved() == 1) {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(url);
+            dispatcher.forward(request, response);
+        } else {
+            RequestDispatcher dispatcher = getServletContext().getRequestDispatcher(errorUrl);
+            dispatcher.forward(request, response);
+        }
+
     }
 }
